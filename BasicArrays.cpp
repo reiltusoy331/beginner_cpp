0c4e72b8e6afe2e7e291e2ca0b73a823b27c75a3
reiltusoy331 <reiltusoy331@gmail.com>
2023-11-20 22:05:31 +0800

    BasicArrays.cpp to my github beginner_cpp repo.

---
+++ BasicArrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int numbers[10] = {1,2,3,4,5,6,7,8,9,10};
+	
+	cout<<"The first element of the array is: "<<numbers[0]<<endl;
+	cout<<"The last element of the array is: "<<numbers[9]<<endl;
+	return 0;
+	
+	
+}
