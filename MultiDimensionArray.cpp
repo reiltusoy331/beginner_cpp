5ae7c3ccb09e81a5b87d72b64443d344dd584deb
reiltusoy331 <reiltusoy331@gmail.com>
2023-11-23 15:50:16 +0800

    MultiDimensionArray.cpp to my github beginner_cpp repo.

---
+++ MultiDimensionArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+		int numbers[3][4]={
+		{0,1,2,3},
+		{4,5,6,7},
+		{8,9,10,11}		
+		};
+		
+		cout<<numbers[0][3];
+		
+	
+	return 0;
+}
