2772420292def70ae336f2334e1ac45e062f02d1
reiltusoy331 <reiltusoy331@gmail.com>
2023-11-30 22:30:44 +0800

    Adding FactorialNumberForLoop.cpp to my github beginner_cpp repo.

---
+++ FactorialNumberForLoop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int number;
+	cout<<"Enter a positive number: ";
+	cin>>number;
+	
+	if (number <0){
+		cout<<"Error! Number is not a positive.";
+	}
+	else{
+		int factorial = 1;
+	for(int i=1; i<=number;i++){		
+		factorial = factorial * i;
+		cout<<i<<" x "<<factorial<<endl;
+	}
+	cout<<"The factorial of "<<number<<": "<<factorial<<endl;	
+}
+	
+	
+		
+	
+	
+	return 0;
+}
