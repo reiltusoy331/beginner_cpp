b6e906d5fe4995c9abf003a9667594ff0b8dbab3
reiltusoy331 <reiltusoy331@gmail.com>
2023-12-14 18:42:24 +0800

    Adding ifelseCommission.cpp to my github beginner_cpp repo.

---
+++ ifelseCommission.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int sales=15001;
+	
+	if(sales <=10000){
+		cout<<"The commission is 10%";
+	}
+	else if(sale <=15000){
+		cout<<"The commission is 15%";		
+	}
+	else{
+		cout<<"The commission is 20%";
+	}
+	
+return 0;
+	
+}
+
+
