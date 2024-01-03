831393abda83f478f80ee17b5305c6e5ca04cf0c
reiltusoy331 <reiltusoy331@gmail.com>
2024-01-03 10:28:56 +0800

    Adding WhileGuessNum.cpp to my github beginner_cpp repo.

---
+++ WhileGuessNum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int secret_number=9;
+	
+	
+	int guess=0;
+	while(guess !=secret_number){
+		cout<<"Guess: ";
+		cin>>guess;
+	}
+	
+	
+	
+	return 0;
+}
