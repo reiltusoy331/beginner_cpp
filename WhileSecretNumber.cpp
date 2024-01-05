7e34f28b74d63f4c0719f5f9f450273188f49763
reiltusoy331 <reiltusoy331@gmail.com>
2024-01-05 19:12:55 +0800

    Adding WhileSecretNumber.cpp to my github beginner_cpp repo.

---
+++ WhileSecretNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int number,secret_number=31;	
+	
+	while(number !=secret_number){
+		cout<<"Guess the secret number: ";
+		cin>>number;
+	}
+	
+	
+	
+	return 0;
+}
