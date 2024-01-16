64a5962c2fbe6065422c0ebc867ef7894f143848
reiltusoy331 <reiltusoy331@gmail.com>
2024-01-16 12:00:57 +0800

    Adding BasicFunctionTwoArgs.cpp to my github beginner_cpp repo.

---
+++ BasicFunctionTwoArgs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void greet(string FirstName, string LastName){
+	cout<<"Hello, "<<FirstName << " "<<LastName<<endl;
+}
+
+int main(){
+	
+greet("Reil","Tusoy");	
+	
+	return 0;
+}
