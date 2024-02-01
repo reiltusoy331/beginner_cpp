bb93c5782adb35753fc2a0044222d2d5a875bc81
reiltusoy331 <reiltusoy331@gmail.com>
2024-02-01 19:37:49 +0800

    Adding SimpleFunction.cpp to my github beginner_cpp repo.

---
+++ SimpleFunction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// Function definition or Function Prototyping
+void greet(string name);
+	
+int main(){
+
+greet("Reil");
+	
+	
+	return 0;
+}
+
+void greet(string name){
+	cout <<"Hello " <<name;
+}
