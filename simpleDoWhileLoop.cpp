e03fb7d8e9716926fc5ce712fa49895703d84be3
reiltusoy331 <reiltusoy331@gmail.com>
2024-02-29 17:46:21 +0800

    Adding simpleDoWhileLoop.cpp to my github beginner_cpp repo.

---
+++ simpleDoWhileLoop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n=1,times=3;
+	
+	do{
+		cout<<"C++ Do While loop "<<n<<endl;
+		n++;
+		
+	}while(n<=times);
+	
+	
+	return 0;
+}
